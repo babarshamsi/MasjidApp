68fa3189f0a93f032d4cde00a016f7eb8ae387d5
Babar <babar@aymcommerce.com>
2025-12-24 17:16:27 +0500

    FEAT: (phase1 done)
    * Login functional issues fixed
    * Submit button changed to async to wait and respond back to login API

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -12,46 +12,78 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-    final authService = AuthService();
-    final emailController = TextEditingController();
-    final passwordController = TextEditingController();
-    var user;
-    bool isLogin = true;
+  final authService = AuthService();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  var user;
+  bool isLogin = true;
 
-    void submit() {
+  Future<void> submit() async {
+    // var user;
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Please enter email and password")),
+      );
+      return;
+    }
+
       if (isLogin) {
-        user = authService.login(emailController.text, passwordController.text);
+        user = await authService.login(
+          emailController.text,
+          passwordController.text,
+        );
       } else {
-        user = authService.signup(emailController.text, passwordController
-            .text);
+        user = await authService.signup(
+          emailController.text,
+          passwordController.text,
+        );
       }
 
       if (user != null && mounted) {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) =>
-        const HomeScreen()),);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const HomeScreen()),
+        );
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text("Invalid credentials")));
       }
     }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(appBar: AppBar(title: Text(isLogin ? "Login" : "Signup")),
-        body: Padding(padding: const EdgeInsets.all(20),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(controller: emailController,
-                decoration: const InputDecoration(hintText: "Email"),),
-              const SizedBox(height: 20,),
-              TextField(controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(hintText: "Password"),),
-              const SizedBox(height: 20,),
-              ElevatedButton(onPressed: submit, child: Text(isLogin ? "Login" : "Signup")),
-              TextButton(onPressed: () =>
-                setState(() => isLogin = !isLogin),
-               child: Text(isLogin ? "Create account" : "Already have account? Login")),
-            ],
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(isLogin ? "Login" : "Signup")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: "Email"),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(hintText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: submit,
+              child: Text(isLogin ? "Login" : "Signup"),
+            ),
+            TextButton(
+              onPressed: () => setState(() => isLogin = !isLogin),
+              child: Text(
+                isLogin ? "Create account" : "Already have account? Login",
+              ),
+            ),
+          ],
         ),
-                );
-    }
+      ),
+    );
   }
+}
