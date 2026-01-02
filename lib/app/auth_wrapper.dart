a2c4fa1a0594c3f9828f752234aff763adb11626
Babar <babar@aymcommerce.com>
2026-01-02 11:37:06 +0500

    FEAT: (Auth module)
    * Signup screen more polished
    * Auth module completed

--- lib/app/auth_wrapper.dart
+++ lib/app/auth_wrapper.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../screens/auth/login_screen.dart';
 import '../screens/auth/signup_screen.dart';
 import '../screens/home/home_screen.dart';
 import '../services/auth_service.dart';
--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -26,40 +26,43 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
     setState(() => isLoading = true);
 
-
-      if (isLogin) {
-        authResult = await authService.login(
-          emailController.text,
-          passwordController.text,
-        );
-      } else {
-        authResult = await authService.signup(
-          emailController.text,
-          passwordController.text,
-        );
-      }
-
-      setState(() => isLoading = false);
-
-      // TODO need to check what is this
-      if(!mounted) return;
-
-     navigateToHomeScreenOrShowError(authResult);
+    if (isLogin) {
+      authResult = await authService.login(
+        emailController.text,
+        passwordController.text,
+      );
+    } else {
+      authResult = await authService.signup(
+        emailController.text,
+        passwordController.text,
+      );
     }
 
-    void navigateToHomeScreenOrShowError(AuthResult authResult) {
-      if (authResult.isSuccessful) {
-        // Auth Wrapper is handling and listening to user stream
-      } else {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text(authResult.errorMessage != null
-       ? authResult.errorMessage!
-        : "Something went wrong")));
-      }
-    }
+    setState(() => isLoading = false);
 
-    bool checkIfEmptyFields(String email, String password) {
+    // TODO need to check what is this
+    if (!mounted) return;
+
+    navigateToHomeScreenOrShowError(authResult);
+  }
+
+  void navigateToHomeScreenOrShowError(AuthResult authResult) {
+    if (authResult.isSuccessful) {
+      // Auth Wrapper is handling and listening to user stream
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            authResult.errorMessage != null
+                ? authResult.errorMessage!
+                : "Something went wrong",
+          ),
+        ),
+      );
+    }
+  }
+
+  bool checkIfEmptyFields(String email, String password) {
     if (email.isEmpty || password.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Please enter email and password")),
@@ -67,40 +70,54 @@ class _SignUpScreenState extends State<SignUpScreen> {
       return true;
     }
     return false;
-    }
-
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(isLogin ? "Login" : "Signup")),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(hintText: "Email"),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(hintText: "Password"),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: isLoading ? null : submit,
-              child: isLoading? const CircularProgressIndicator() : Text(isLogin ? "Login" : "Signup"),
-            ),
-            TextButton(
-              onPressed: () => setState(() => isLogin = !isLogin),
-              child: Text(
-                isLogin ? "Create account" : "Already have account? Login",
+      body: Center(
+        child: Container(
+          width: 350,
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(isLogin ? "Login" : "Signup", style: TextStyle(fontSize: 36,
+                  fontWeight:
+              FontWeight.bold),),
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(border: OutlineInputBorder
+                  (), hintText: "Email"),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 20),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(border: OutlineInputBorder
+                  (), hintText: "Password"),
+              ),
+
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: isLoading ? null : submit,
+                child: isLoading
+                    ? const CircularProgressIndicator()
+                    : Text(isLogin ? "Login" : "Signup"),
+              ),
+              SizedBox(height: 10,),
+
+              TextButton(
+                onPressed: () => setState(() => isLogin = !isLogin),
+                child: Text(
+                  isLogin ? "Create account" : "Already have account? Login",
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
